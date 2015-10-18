4be414ec110281ffabf2c0e6c220613c135cf030
prmelehan <ryan@Ryans-Mac-Pro.local>
2015-10-18 16:20:07 -0400

    Started first steps to make full version
    Added a shifter for the given text to change it’s state

---
+++ de-enc.py
@@ -0,0 +1,42 @@
+import math
+
+
+# A script that creates a Poly Alphabetic Cipher
+
+sentence = input("Gimme your phrase: ")
+
+letters = list(sentence)
+
+#Shift Right Ord
+first = letters[0]
+first = ord(first)
+print(first)
+#Shift Left Ord
+last = letters[-1]
+last = ord(last)
+print(last)
+ordinated = []
+for letter in letters:
+	#Converting all letters to their Ordinate Values in ASCII
+	ordinatedLetter = ord(letter)
+	ordinated.append(ordinatedLetter)
+print(ordinated)
+#Shift all the numbers by the Ordinated value of the first letter
+shiftRightOrdinated = []
+for number in ordinated:
+	shiftNum = (number - first) % 127
+	shiftRightOrdinated.append(shiftNum)
+print(shiftRightOrdinated)
+shiftLeftOrdinated = []
+
+for number in shiftRightOrdinated:
+	shiftNum = (number + last) % 127
+	shiftLeftOrdinated.append(shiftNum)
+print(shiftLeftOrdinated)
+encString = []
+
+for number in shiftLeftOrdinated:
+	char = str(chr(number))
+	encString.append(char)
+print(encString)
+print(''.join(encString))
