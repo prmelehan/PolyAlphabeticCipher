84ae14a0c2badf7ed86ff81099eb798903721df4
prmelehan <ryan@Katherines-MacBook-Pro.local>
2015-10-20 19:22:55 -0400

    Added Dictionary A
    Added the first Dictionary in the multi-dictionary series. Index starts
    at zero, ends at 84 or so.

--- de-enc.py
+++ de-enc.py
@@ -7,36 +7,49 @@ sentence = input("Gimme your phrase: ")
 
 letters = list(sentence)
 
-#Shift Right Ord
+PolyDictA = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21,'w':22,'x':23,'y':24,'z':25, 'A':26, 'B':27,'C':28,'D':29,'E':30,'F':31,'G':32,'H':33,'I':34,'J':35,'K':36,'L':37,'M':38,'N':39,'O':40,'P':41,'Q':42,'R':43,'S':44,'T':45,'U':46,'V':47,'W':48,'X':49,'Y':50,'Z':51,'1':52,'2':53,'3':54,'4':55,'5':56,'6':57,'7':58,'8':59,'9':60,'0':61,'!':62,'@':63,'#':64,'$':65,'%':66,'^':67,'&':68,'*':69,'(':70,')':71,'-':72,'+':73,'=':74,':':75,';':76,'"':77,"'":78,',':79,'<':80,'>':81,'.':82,'?':83,' ':84}
+#PolyDictB = {'﷼','₺','»','℞','₦','₯','৳','฿'}
+# A function that replaces find_valueA with a custom indexing function for the PolyDictA dictionary
+
+def find_valueA(letter):
+	letter = str(letter)
+	key = PolyDictA[letter]
+	return key
+def find_keyA(val):
+    for key in PolyDictA.keys():
+    	if PolyDictA[key] == val:
+    		return str(key)
+# Shift Right Ord
 first = letters[0]
-first = ord(first)
-print(first)
+first = find_valueA(first)
+#print(first)
 #Shift Left Ord
 last = letters[-1]
-last = ord(last)
-print(last)
+last = find_valueA(last)
+#print(last)
 ordinated = []
 for letter in letters:
 	#Converting all letters to their Ordinate Values in ASCII
-	ordinatedLetter = ord(letter)
+	ordinatedLetter = find_valueA(letter)
 	ordinated.append(ordinatedLetter)
-print(ordinated)
+#print(ordinated)
 #Shift all the numbers by the Ordinated value of the first letter
 shiftRightOrdinated = []
 for number in ordinated:
-	shiftNum = (number - first) % 127
+	shiftNum = (number - first) % 84
 	shiftRightOrdinated.append(shiftNum)
-print(shiftRightOrdinated)
+#print(shiftRightOrdinated)
 shiftLeftOrdinated = []
 
 for number in shiftRightOrdinated:
-	shiftNum = (number + last) % 127
+	shiftNum = (number + last) % 84
 	shiftLeftOrdinated.append(shiftNum)
-print(shiftLeftOrdinated)
+#print(shiftLeftOrdinated)
 encString = []
 
 for number in shiftLeftOrdinated:
-	char = str(chr(number))
+	char = find_keyA(number)
 	encString.append(char)
-print(encString)
+#print(encString)
 print(''.join(encString))
+
