10ea1762de3d344066c95efec744355938641138
prmelehan <ryan@Katherines-MacBook-Pro.local>
2015-11-26 16:27:02 -0500

    Completed
    All code complete. Possible future improvements, UI improvements and
    clarification improvements.

--- enc.py
+++ enc.py
@@ -1,5 +1,10 @@
-import math
+#import system for Python version check
+import sys
 
+#Check to see if python version is version 3
+
+if(sys.version_info[0] < 3):
+	exit("Python 3 is required")
 
 # A script that creates a Poly Alphabetic Cipher
 
@@ -11,8 +16,8 @@ shiftletters = list(shiftword)
 #Same as above comment____________^^
 letters = list(sentence)
 #Checks to see if there was even input
-if(len(shiftletters) == 0):
-	exit()
+if(len(shiftletters) == 0 or len(sentence) == 0):
+	exit("Both a passphrase and a phrase to be encoded is required")
 #print(letters)
 PolyDictA = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21,'w':22,'x':23,'y':24,'z':25, 'A':26, 'B':27,'C':28,'D':29,'E':30,'F':31,'G':32,'H':33,'I':34,'J':35,'K':36,'L':37,'M':38,'N':39,'O':40,'P':41,'Q':42,'R':43,'S':44,'T':45,'U':46,'V':47,'W':48,'X':49,'Y':50,'Z':51,'1':52,'2':53,'3':54,'4':55,'5':56,'6':57,'7':58,'8':59,'9':60,'0':61,'!':62,'@':63,'#':64,'$':65,'%':66,'^':67,'&':68,'*':69,'(':70,')':71,'-':72,'+':73,'=':74,':':75,';':76,'"':77,"'":78,',':79,'<':80,'>':81,'.':82,'?':83,' ':84}
 # A function that replaces find_valueA with a custom indexing function for the PolyDictA dictionary
